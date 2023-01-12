676733f99acc5e1a6772e0a41bb50b593d64d459
HJ <12210544a@inha.edu>
2023-01-12 16:53:03 +0900

    01 temperature

---
+++ main.py
@@ -0,0 +1,8 @@
+# (100°F − 32) × 5/9 = 37.778°C
+fahrenheit = float(input('화씨 온도 : '))
+# 변수 : 가독성 좋게 설정, 의미를 알 수 있게 (fahrenheit=fh,etc)
+celsius = (fahrenheit - 32.0) * (5.0/9.0)
+# print(celsius)
+# print(f'화씨 온도 {fahrenheit}도는 섭씨 온도{celsius}입니다')
+
+
